f6fd3832d726f7ad40fa44c9269d4da9d0329eff
dominik.dusinski <dominik.dusinski@gmail.com>
2025-07-28 20:59:55 +0200

    Unmarshalling of Payment from finastra message

--- src/main/java/org/example/controller/MsgController.java
+++ src/main/java/org/example/controller/MsgController.java
@@ -1,6 +1,13 @@
 package org.example.controller;
 
+import jakarta.xml.bind.*;
+import org.example.model.com.fundtech.scl.commontypes.FndtMsgHeaderType;
+import org.example.model.com.fundtech.scl.commontypes.FndtMsgType;
+import org.example.model.com.fundtech.scl.commontypes.PmntType;
 import org.springframework.web.bind.annotation.*;
+
+import javax.xml.namespace.QName;
+import java.io.StringReader;
 
 @RestController
 @RequestMapping
@@ -16,5 +23,36 @@ public class MsgController {
         System.out.println(str);
     }
 
+    @PostMapping("/msg")
+    public void getMessage2(@RequestBody String str) throws JAXBException {
+        System.out.println("received message: \n" + str);
 
+//        JAXBContext jaxbContext = JAXBContext.newInstance(FndtMsgType.class);
+        JAXBContext jaxbContext = JAXBContext.newInstance("org.example.model.com.fundtech.scl.commontypes");
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        StringReader stringReader = new StringReader(str);
+
+        JAXBElement jaxbElement = (JAXBElement) unmarshaller.unmarshal(stringReader);
+
+
+        FndtMsgType fndtMsgType = (FndtMsgType)jaxbElement.getValue();
+
+        System.out.println(fndtMsgType.getMsg().getPmnt().getAny());
+
+        Marshaller marshaller = jaxbContext.createMarshaller();
+//        marshaller.marshal(jaxbElement, System.out);
+
+//        marshaller.marshal(new JAXBElement<FndtMsgHeaderType>(
+//                new QName("uri","local"),
+//                FndtMsgHeaderType.class,
+//                ((FndtMsgType) jaxbElement.getValue()).getHeader()
+//        ), System.out);
+
+        marshaller.marshal(new JAXBElement<PmntType>(
+                new QName("uri","local"),
+                        PmntType.class,
+                        ((FndtMsgType) jaxbElement.getValue()).getMsg().getPmnt()
+                ),System.out);
+
+    }
 }
