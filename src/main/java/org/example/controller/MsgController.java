4a4b98293f5d5d344f458e98706b38ca0a25a26d
dominik.dusinski <dominik.dusinski@gmail.com>
2025-08-02 21:42:16 +0200

    Marshalling Instant Payment

--- src/main/java/org/example/controller/MsgController.java
+++ src/main/java/org/example/controller/MsgController.java
@@ -1,10 +1,12 @@
 package org.example.controller;
 
 import jakarta.xml.bind.*;
+import org.example.model.InstantPayment;
 import org.example.model.fin.com.fundtech.scl.commontypes.FndtMsgType;
 import org.example.model.avq.pacs.iso.std.iso._20022.tech.xsd.pacs_008_001.Document;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
 import java.io.StringReader;
 
@@ -26,7 +28,11 @@ public class MsgController {
     public void getMessage2(@RequestBody String str) throws JAXBException {
         System.out.println("Incoming message: \n" + str);
 
-        JAXBContext jaxbContext = JAXBContext.newInstance("org.example.model.avq.pacs.iso.std.iso._20022.tech.xsd.pacs_008_001:org.example.model.fin.com.fundtech.scl.commontypes");
+        JAXBContext jaxbContext = JAXBContext.newInstance("org.example.model.avq.pacs.iso.std.iso._20022.tech.xsd.pacs_008_001:org.example.model.fin.com.fundtech.scl.commontypes", InstantPayment.class.getClassLoader());
+
+        JAXBContext jaxbContext2 = JAXBContext.newInstance(InstantPayment.class);
+        Marshaller marshaller2 = jaxbContext2.createMarshaller();
+
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
         StringReader stringReader = new StringReader(str);
 
@@ -41,6 +47,10 @@ public class MsgController {
         Document outPacs008 = new Document();
         outPacs008.setFIToFICstmrCdtTrf(inPacs008.getFIToFICstmrCdtTrf());
 
+        InstantPayment outInstantPayment = new InstantPayment();
+        outInstantPayment.setDocument(inPacs008);
+
+
         Marshaller marshaller = jaxbContext.createMarshaller();
 //        marshaller.marshal(new JAXBElement<PmntType>(
 //                new QName("uri","local"),
@@ -49,10 +59,16 @@ public class MsgController {
 //                ),System.out);
 
         System.out.println("Outgoing message:");
-        marshaller.marshal(new JAXBElement<Document>(
+//        marshaller.marshal(new JAXBElement<Document>(
+//                new QName("uri","local"),
+//                Document.class,
+//                outPacs008
+//        ),System.out);
+
+                marshaller2.marshal(new JAXBElement<InstantPayment>(
                 new QName("uri","local"),
-                Document.class,
-                outPacs008
+                InstantPayment.class,
+                outInstantPayment
         ),System.out);
 
     }
--- src/main/java/org/example/model/InstantPayment.java
+++ src/main/java/org/example/model/InstantPayment.java
@@ -4,6 +4,7 @@ import jakarta.xml.bind.annotation.*;
 import jakarta.xml.bind.annotation.XmlType;
 import org.example.model.avq.hdr.Hdr;
 import org.example.model.avq.pacs.iso.std.iso._20022.tech.xsd.pacs_008_001.Document;
+import org.example.model.avq.hdr.ObjectFactory;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
@@ -12,12 +13,24 @@ import org.example.model.avq.pacs.iso.std.iso._20022.tech.xsd.pacs_008_001.Docum
 })
 
 
-@XmlRootElement(name = "InstantPayment", namespace = "")
+@XmlRootElement(name = "InstantPayment")
 public class InstantPayment {
     @XmlElement(name = "Hdr", namespace = "", required = true)
     protected Hdr hdr;
     @XmlElement(name = "Document", required = true)
     protected Document document;
+
+    {
+        ObjectFactory hdrObjectFactory = new ObjectFactory();
+        this.hdr = hdrObjectFactory.createHdr();
+        this.hdr.setDirection("IN");
+        Hdr.Ident id = hdrObjectFactory.createHdrIdent();
+        id.setTp("BICFI");
+        id.setVal("XAVALULU");
+
+        this.hdr.getIdent().add(id);
+            }
+
 
     public Hdr getHdr() {
         return hdr;
